65d14f7aac5151f9c471f2a6f8f69570679dc122
Aidar Ibraev <aidaribraev32@gmail.com>
2024-03-16 06:42:24 +0600

    commit - 4. Added styled-components library

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,51 @@
 import React from 'react';
-import './App.css';
+import styled from "styled-components";
 
-function App() {
-  return (
-    <div>
-      This is App component))
-    </div>
-  );
+const App = () => {
+    return (
+        <div>
+            This is App component))
+            <Box>
+                <Link>Super Link</Link>
+                <StyledButton as="a" href="#">link</StyledButton>
+                <StyledButton>button</StyledButton>
+                <SuperButton>yoyo</SuperButton>
+            </Box>
+        </div>
+    );
 }
 
 export default App;
 
 
+const StyledButton = styled.button`
+    border: none;
+    background-color: darkblue;
+    padding: 10px 20px;
+    border-radius: 5px;
+    color: snow;
+    font-size: 2rem;
+    font-weight: bold;
+`
+
+const Link = styled.a `
+    border: none;
+    background-color: darkblue;
+    padding: 10px 20px;
+    border-radius: 5px;
+    color: snow;
+    font-size: 2rem;
+    font-weight: bold;
+`
+
+const SuperButton = styled(StyledButton)`
+    background-color: darkcyan;
+`
+
+const Box = styled.div`
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+`
